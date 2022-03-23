fb6bdabc83835ad1ab89c1de1d7f944a0fcbc820
SadMuMu <289926353@qq.com>
2022-03-23 20:31:16 +0800

    收集四个组件的数据到record

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,23 +2,20 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input
-        type="text"
-        v-model="value"
-        placeholder="在这里添加备注"
-      />
+      <input type="text" v-model="value" placeholder="在这里添加备注" />
     </label>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 @Component
 export default class Notes extends Vue {
   value = "";
-  onInput(event:KeyboardEvent){
-    this.value=(event.target as HTMLInputElement).value;
+  @Watch("value")
+  onValueChange(value: string) {
+    this.$emit("update:value", value);
   }
 }
 </script>
