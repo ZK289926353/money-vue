2990a0f7b555089756cea16f0d9f6a10b5f68930
SadMuMu <289926353@qq.com>
2022-03-24 20:57:26 +0800

    展示和创建标签

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
+  background: #f5f5f5;
   font-size: 16px;
   font-family: $font-hei;
   line-height: 1.5;
