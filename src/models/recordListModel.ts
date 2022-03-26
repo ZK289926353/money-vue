996967395dd133e6611dc0008bbe6e1a38628f54
SadMuMu <289926353@qq.com>
2022-03-26 10:47:53 +0800

    将create封装到recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,17 +1,21 @@
+import clone from "@/lib/clone";
+
 const localStorageKeyName = "recordList";
 const recordListModel = {
-  data:[] as RecordItem[],
+  data: [] as RecordItem[],
   fetch() {
-    this.data=JSON.parse(
+    this.data = JSON.parse(
       window.localStorage.getItem(localStorageKeyName) || "[]"
     ) as RecordItem[];
     return this.data;
   },
+  create(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
+  },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
-  },
-  clone(data: RecordItem[] | RecordItem) {
-    return JSON.parse(JSON.stringify(data));
   },
 };
 
