9d76b228293f368a18734474938e4685c263d01d
SadMuMu <289926353@qq.com>
2022-03-26 11:05:24 +0800

    封装window.createTag

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,7 +1,25 @@
 type RecordItem = {
-    tags: string[];
-    notes: string;
-    type: "-" | "+";
-    amount: number;
-    createdAt?: Date;
-  };
+  tags: string[];
+  notes: string;
+  type: "-" | "+";
+  amount: number;
+  createdAt?: Date;
+};
+
+type Tag = {
+  id: string;
+  name: string;
+};
+type TagListModel = {
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => "success" | "duplicated"; //success表示成功，duplicated表示name重复
+  save: () => void;
+  update: (id: string, name: string) => "success" | "not found" | "duplicated";
+  remove: (id: string) => boolean;
+};
+
+interface Window {
+  tagList: Tag[];
+  createTag: (name: string) => void;
+}
