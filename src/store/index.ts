ff55113e2a12f6ea4909c5d0284095793b73f994
SadMuMu <289926353@qq.com>
2022-04-01 19:50:54 +0800

    修复弹出alertbug

--- src/store/index.ts
+++ src/store/index.ts
@@ -44,8 +44,9 @@ const store = new Vuex.Store({
     },
     createTag(state, name: string) {
       const names = state.tagList.map((item) => item.name);
-      if (names.indexOf(name) >= 0) {
+      if (names.indexOf(name.trim()) >= 0) {
         window.alert("标签名重复");
+        return;
       }
       const id = createId().toString();
       state.tagList.push({ id, name: name });
