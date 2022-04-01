89eed175abfd138674fa47385c96c3e7186f8941
SadMuMu <289926353@qq.com>
2022-04-01 20:15:28 +0800

    优化PC样式

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,12 @@ new Vue({
   render: (h) => h(App),
 }).$mount("#app");
 
-window.onload=function(){
-  setTimeout(function(){
-    window.scrollTo(0,9999);
-  },0)
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 9999);
+  }, 0);
+};
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert("请使用手机打开本页面，以保证浏览效果");
 }
